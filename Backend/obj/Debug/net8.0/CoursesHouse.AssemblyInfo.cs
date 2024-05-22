9d2970f05e658d4e2e32387fa0256ac81b147858
emek-k <emakorycki@gmail.com>
2024-05-23 00:35:20 +0200

    Created second basic version of frontend called "frontend-fresh" with basic subsites and navigation.

--- Backend/obj/Debug/net8.0/CoursesHouse.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/CoursesHouse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoursesHouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0de2c1a6bde16458e036b55f2f43d9f289aec144")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f08ab3bbf2f6fdb0beeb912641fe1a831e7ba0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoursesHouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoursesHouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
