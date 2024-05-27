99d71787070454c7d9910be8038d9c6aeadba054
Dawid Dluski <dawiddluski5@gmail.com>
2024-05-27 12:00:45 +0200

    dodanie logout oraz poprawiony widok strony

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,30 +1,48 @@
 import React from "react";
 import logo from "./logo.webp";
 import "./Navbar.css";
+import { useAuth } from "../../Context/useAuth";
+import { Link } from "react-router-dom";
 
 interface Props {}
 
 const Navbar = (props: Props) => {
+  const { isLoggedIn, user,logout} = useAuth();
   return (
     <nav className="relative container w-16 mx-auto p-6">
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-20">
           <img src={logo} alt="" />
           <div className="hidden font-bold lg:flex">
-            <a href="" className="text-black hover:text-darkBlue ml-8">
+            {/* <a href="" className="text-black hover:text-darkBlue ml-8">
               Dashboard
-            </a>
+            </a> */}
           </div>
         </div>
-        <div className="hidden lg:flex items-center space-x-6 text-back">
-          <div className="hover:text-darkBlue ">Logowanie</div>
-          <a
-            href=""
+        {isLoggedIn() ? (
+           <div className="hidden lg:flex items-center space-x-6 text-back">
+           <div className="hover:text-darkBlue ">Witaj, {user?.userName}</div>
+           <a
+             onClick={logout}
+             className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
+           >
+             Wyloguj się
+           </a>
+         </div>
+
+        ) : (
+          <div className="hidden lg:flex items-center space-x-6 text-back">
+          <Link to="/login" className="hover:text-darkBlue ">Logowanie</Link>
+          <Link to="/register"
             className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
           >
             Rejestracja
-          </a>
+          </Link>
         </div>
+
+          
+        )}
+
       </div>
     </nav>
   );
